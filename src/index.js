26ccef0a0bfdce14cfb1dd551b336f6637456370
sahajshukla <sahajshukla@gmail.com>
2024-04-14 20:43:33 -0400

    removed stupid logging

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./components/App";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(process.env.REACT_APP_FIREBASE_API_KEY)
 root.render(
   <React.StrictMode>
     
