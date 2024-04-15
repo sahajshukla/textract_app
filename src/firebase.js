a4748cdac375ccb62cea055fb2f783907d0cbcca
sahajshukla <sahajshukla@gmail.com>
2024-04-14 20:54:45 -0400

    latest one

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-const app = firebase.initializeApp({
-    apiKey: "AlzaSyDNYlowcEpXtCE5HKQ1kK4hPmVSUcwTtS4",
-    authDomain: "ocr-textract-dev.firebaseapp.com",
-    projectId: "ocr-textract-dev",
-    storageBucket: "ocr-textract-dev.appspot.com",
-    messagingSenderId: "346774948760",
-    appId: "1:346774948760:web:c99a87bb4a323be1cff9ed"
-})
+const firebaseConfig = {
+  apiKey: "AIzaSyDNYlowcEpXtCE5HKQ1kK4hPmVSUcwTtS4",
+  authDomain: "ocr-textract-dev.firebaseapp.com",
+  projectId: "ocr-textract-dev",
+  storageBucket: "ocr-textract-dev.appspot.com",
+  messagingSenderId: "346774948760",
+  appId: "1:346774948760:web:c99a87bb4a323be1cff9ed",
+  measurementId: "G-5WJTSNQD7T"
+};
 
-export const auth = app.auth()
-export default app
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
